dbc472582df1db4ad5b4efbb94f7c45a45641113
Kevin <kevin@tabolo.de>
2015-03-09 00:50:15 +0100

    hack to make it work with new versions of mongoid

--- lib/rails/generators/mongoid/mongoid_generator.rb
+++ lib/rails/generators/mongoid/mongoid_generator.rb
@@ -14,11 +14,7 @@ module Mongoid #:nodoc:
       # Implement the required interface for Rails::Generators::Migration.
       def self.next_migration_number(dirname) #:nodoc:
         next_migration_number = current_migration_number(dirname) + 1
-        if Mongoid.configure.timestamped_migrations
-          [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
-        else
-          "%.3d" % next_migration_number
-        end
+        [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
       end
 
     end
